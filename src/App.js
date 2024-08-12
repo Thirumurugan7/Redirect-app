f0b1ef8bc9bef3ebbaaa4a1b6fa5c608102bdfb3
vishalmet <vishalmet003@gmail.com>
2024-08-12 21:47:12 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeaderLogo from './components/Header'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center mx-auto bg-gradient-to-t from-customStart via-customStart to-blue-950 min-h-screen'>
+      <HeaderLogo />
+      <div>
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
